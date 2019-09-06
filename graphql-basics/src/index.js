78f9cfb1bfee9db7698f58d7bd642d99997e474b
Jacob Chae <jbcden@gmail.com>
2019-09-06 11:51:32 +0900

    Export challenge

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,8 @@
 import myCurrentLocation, { message, name, getGreeting } from './myModule'
+import myAdd, { subtract } from './math'
 
 console.log(message, myCurrentLocation)
 console.log(getGreeting(name))
+
+console.log(myAdd(1, 99))
+console.log(subtract(101, 1))
---
+++ graphql-basics/src/math.js
@@ -0,0 +1,9 @@
+const add = (a, b) => {
+    return a + b
+}
+
+const subtract = (a, b) => {
+    return a - b
+}
+
+export { subtract, add as default}
