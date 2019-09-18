cdee32b6a194570db6aca70ddf12b193a43a1047
Jacob Chae <jbcden@gmail.com>
2019-09-18 19:06:31 +0900

    Move graphql types into their own file
    This also gives us some nice syntax highlighting instead of just seeing
    these used in a string.

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -64,69 +64,6 @@ let comments = [{
     post: '3'
 }]
 
-// Type definitions (schema)
-const typeDefs = `
-    type Query {
-        posts(query: String): [Post!]!
-        users(query: String): [User!]!
-        me: User!
-        post: Post!
-        comments: [Comment!]!
-    }
-
-    type Mutation {
-      createUser(data: CreateUserInput!): User!
-      deleteUser(id: ID!): User!
-      createPost(data: CreatePostInput!): Post!
-      deletePost(id: ID!): Post!
-      createComment(data: CreateCommentInput!): Comment!
-      deleteComment(id: ID!): Comment!
-    }
-
-    input CreatePostInput {
-      title: String!
-      body: String!
-      published: Boolean!
-      author: ID!
-    }
-
-    type Post {
-        id: ID!
-        title: String!
-        body: String!
-        published: Boolean!
-        author: User!
-        comments: [Comment!]!
-    }
-
-    input CreateUserInput {
-      name: String!
-      email: String
-      age: Int
-    }
-
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        age: Int
-        posts: [Post!]!
-        comments: [Comment!]!
-    }
-
-    input CreateCommentInput {
-      text: String!
-      author: ID!
-      post: ID!
-    }
-
-    type Comment {
-        id: ID!
-        text: String!
-        author: User!
-        post: Post!
-    }
-`
 
 // Resolvers
 const resolvers = {
@@ -320,8 +257,8 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
+  typeDefs: './src/schema.graphql',
+  resolvers
 })
 
 server.start(() => {
